35ad191f6188472125def5913ea3b66ceb058bd5
Bagas Udi Sahsangka <udibagas@gmail.com>
2018-03-09 01:16:11 +0700

    untuk perhitungan pue

--- less/jfss.php
+++ less/jfss.php
@@ -1,22 +1,20 @@
 <?php
-
 header("Content-type: text/json");
+include_once '../koneksi.php';
 
-
- include_once '../koneksi.php';
-          $sql=$konek->query("SELECT (SELECT COUNT(sensor.ID_SENSOR) FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE sensor.PARAMETER='gas' AND pesan<>'' AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR ) ) as k_gas,
+$sql=$konek->query("SELECT (SELECT COUNT(sensor.ID_SENSOR) FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE sensor.PARAMETER='gas' AND pesan<>'' AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR ) ) as k_gas,
 (SELECT COUNT(sensor.ID_SENSOR) FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE sensor.PARAMETER='suhu' AND pesan<>'' AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR ) ) as k_suhu,
 (SELECT COUNT(sensor.ID_SENSOR) FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE sensor.PARAMETER='air' AND pesan<>'' AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR ) ) as k_air,
 (SELECT COUNT(sensor.ID_SENSOR) FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE sensor.PARAMETER='lembab' AND pesan<>'' AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )) as k_lembab,
 (SELECT SUM(trans5.nilai) as jumlah FROM trans5 INNER JOIN sensor ON trans5.ID_SENSOR = sensor.ID_SENSOR WHERE LEFT(sensor.PARAMETER,6)='active' AND LEFT(sensor.POSISIDETAIL,9)='panel MSB'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )) as k_daya
 ,(((SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='15'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='34'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 +
 (SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='16'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='35'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 +
 (SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='17'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='36'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 )
 
 
@@ -24,20 +22,46 @@ trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_
 
 (
 (SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='31'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='51'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 +
 (SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='32'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='52'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 +
 (SELECT AVG((trans5.nilai)) as nilai FROM
-trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='33'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='53'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='67'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='68'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='82'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='83'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='97'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='98'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='112'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='113'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
++
+(SELECT AVG((trans5.nilai)) as nilai FROM
+trans5 INNER JOIN sensor ON sensor.ID_SENSOR = trans5.ID_SENSOR WHERE sensor.ID_SENSOR='114'  AND trans5.id IN (SELECT MAX(id)AS id FROM trans5  GROUP BY ID_SENSOR )  group by sensor.ID_SENSOR)
 )
-
 ) as PUE FROM trans5 LIMIT 1");
-          $data=$sql->fetch_assoc();
+
+$data=$sql->fetch_assoc();
 
 $ret = $data;
 $konek->close();
 echo json_encode($ret);
 ?>
- 
